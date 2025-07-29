a6c9a43e811dbced9adb58286136fe3b77bebd86
Charlie Van Hook <62162108+Charmander542@users.noreply.github.com>
2025-07-29 15:30:20 -0400

    Update single_hop.py

--- single_hop.py
+++ single_hop.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Hopping (Final Corrected Version)
+# Title: Hopping (Final Version)
 # Generated: Thu Jul 24 17:26:10 2025
 ##################################################
 
@@ -24,18 +24,19 @@ from gnuradio import uhd
 from gnuradio.fft import window
 from gnuradio.filter import firdes
 from gnuradio.wxgui import fftsink2
-from grc_gnuradio import wxgui as grc_wxgui
+from grc_gnuradio import wxgui as grc_wxgui # This import defines 'grc_wxgui'
 from optparse import OptionParser
 import lora
 import osmosdr
-import pmt  # PMT is needed for messaging
+import pmt
 import wx
 
 
-class single(grc_gnuradio.wxgui.top_block_gui):
+# Corrected class definition to use the imported 'grc_wxgui' name
+class single(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_gnuradio.wxgui.top_block_gui.__init__(self, title="Hopping")
+        grc_wxgui.top_block_gui.__init__(self, title="Hopping")
 
         ##################################################
         # Variables
@@ -46,8 +47,12 @@ class single(grc_gnuradio.wxgui.top_block_gui):
         self.target_freq = target_freq = [902.3e6, 902.5e6, 902.7e6, 902.9e6, 903.1e6, 903.3e6, 903.5e6, 903.7e6, 903.9e6, 904.1e6]
         self.hop_interval = hop_interval = 1000 # 1 second
         self.freq_index = freq_index = 0
-        self.capture_freq = capture_freq = 903e6 # Fixed center frequency for the USRP
-        # (other variables)
+        self.capture_freq = capture_freq = 903e6
+        self.symbols_per_sec = symbols_per_sec = float(bw) / (2**sf)
+        self.firdes_tap = firdes_tap = firdes.low_pass(1, samp_rate, bw, 10000, firdes.WIN_HAMMING, 6.67)
+        self.downlink = downlink = False
+        self.decimation = decimation = 1
+        self.bitrate = bitrate = sf * (1 / (2**sf / float(bw)))
 
         ##################################################
         # Blocks
@@ -76,12 +81,11 @@ class single(grc_gnuradio.wxgui.top_block_gui):
         	),
         )
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(capture_freq, 0) # Set USRP frequency only once
+        self.uhd_usrp_source_0.set_center_freq(capture_freq, 0)
         self.uhd_usrp_source_0.set_gain(20, 0)
         
         self.lora_message_socket_sink_0 = lora.message_socket_sink('127.0.0.1', 40868, 0)
-        # Initialize LoRa receiver with the first frequency in the list
-        self.lora_lora_receiver_0 = lora.lora_receiver(samp_rate, self.target_freq[self.freq_index], ([self.target_freq[self.freq_index]]), bw, sf, False, 4, True, False, False, 1, False, False)
+        self.lora_lora_receiver_0 = lora.lora_receiver(samp_rate, capture_freq, ([self.target_freq[self.freq_index]]), bw, sf, False, 4, True, False, downlink, decimation, False, False)
         
         ##################################################
         # Timer for frequency hopping
@@ -110,41 +114,24 @@ class single(grc_gnuradio.wxgui.top_block_gui):
         self.freq_index = (self.freq_index + 1) % len(self.target_freq)
         new_freq = self.target_freq[self.freq_index]
 
-        # STEP 1: Create an empty PMT dictionary
         msg_dict = pmt.make_dict()
-        # STEP 2: Add the key-value pair for the new frequency
         msg_dict = pmt.dict_add(msg_dict, pmt.intern("freq"), pmt.from_double(new_freq))
         
-        # STEP 3: Publish the correctly formed message to the block's command port
         # Note: The port name is assumed to be 'command'. If this fails, try 'cmd'.
         self.lora_lora_receiver_0.message_port_pub(pmt.intern("command"), msg_dict)
         
-        # DO NOT retune the USRP source here. The LoRa block handles it.
-        
-        print("Hopping to frequency: %.2f MHz" % (new_freq / 1e6))
+        print("Hopping LoRa channel to: %.2f MHz" % (new_freq / 1e6))
 
 
     # --- Getter/Setter Methods ---
-    def get_target_freq(self):
-        return self.target_freq
-
-    def set_target_freq(self, target_freq):
-        self.target_freq = target_freq
-        self.freq_index = 0
-        # When the list is changed, command the receiver to go to the first new frequency
-        new_initial_freq = self.target_freq[self.freq_index]
-        
-        msg_dict = pmt.make_dict()
-        msg_dict = pmt.dict_add(msg_dict, pmt.intern("freq"), pmt.from_double(new_initial_freq))
-        self.lora_lora_receiver_0.message_port_pub(pmt.intern("command"), msg_dict)
-
-    # (Other getters and setters as before)
     def get_sf(self):
         return self.sf
 
     def set_sf(self, sf):
         self.sf = sf
+        self.set_symbols_per_sec(float(self.bw) / (2**self.sf))
         self.lora_lora_receiver_0.set_sf(self.sf)
+        self.set_bitrate(self.sf * (1 / (2**self.sf / float(self.bw))))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -153,12 +140,51 @@ class single(grc_gnuradio.wxgui.top_block_gui):
         self.samp_rate = samp_rate
         self.wxgui_fftsink2_1.set_sample_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.set_firdes_tap(firdes.low_pass(1, self.samp_rate, self.bw, 10000, firdes.WIN_HAMMING, 6.67))
 
     def get_bw(self):
         return self.bw
 
     def set_bw(self, bw):
         self.bw = bw
+        self.set_symbols_per_sec(float(self.bw) / (2**self.sf))
+        self.set_firdes_tap(firdes.low_pass(1, self.samp_rate, self.bw, 10000, firdes.WIN_HAMMING, 6.67))
+        self.set_bitrate(self.sf * (1 / (2**self.sf / float(self.bw))))
+
+    def get_target_freq(self):
+        return self.target_freq
+
+    def set_target_freq(self, target_freq):
+        self.target_freq = target_freq
+        self.freq_index = 0
+        new_initial_freq = self.target_freq[self.freq_index]
+        msg_dict = pmt.make_dict()
+        msg_dict = pmt.dict_add(msg_dict, pmt.intern("freq"), pmt.from_double(new_initial_freq))
+        self.lora_lora_receiver_0.message_port_pub(pmt.intern("command"), msg_dict)
+
+    def get_symbols_per_sec(self):
+        return self.symbols_per_sec
+
+    def set_symbols_per_sec(self, symbols_per_sec):
+        self.symbols_per_sec = symbols_per_sec
+
+    def get_firdes_tap(self):
+        return self.firdes_tap
+
+    def set_firdes_tap(self, firdes_tap):
+        self.firdes_tap = firdes_tap
+
+    def get_downlink(self):
+        return self.downlink
+
+    def set_downlink(self, downlink):
+        self.downlink = downlink
+
+    def get_decimation(self):
+        return self.decimation
+
+    def set_decimation(self, decimation):
+        self.decimation = decimation
 
     def get_capture_freq(self):
         return self.capture_freq
@@ -167,6 +193,12 @@ class single(grc_gnuradio.wxgui.top_block_gui):
         self.capture_freq = capture_freq
         self.wxgui_fftsink2_1.set_baseband_freq(self.capture_freq)
         self.uhd_usrp_source_0.set_center_freq(self.capture_freq, 0)
+
+    def get_bitrate(self):
+        return self.bitrate
+
+    def set_bitrate(self, bitrate):
+        self.bitrate = bitrate
 
 
 def main(top_block_cls=single, options=None):
@@ -178,6 +210,7 @@ def main(top_block_cls=single, options=None):
         print "Error starting flowgraph: %s" % e
         import traceback
         traceback.print_exc()
+
 
 if __name__ == '__main__':
     main()
