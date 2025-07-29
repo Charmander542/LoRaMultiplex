d29a40aaed31398fa7227f9c3e0b36829cc543b1
Charlie Van Hook <62162108+Charmander542@users.noreply.github.com>
2025-07-29 15:15:13 -0400

    Update single_hop.py

--- single_hop.py
+++ single_hop.py
@@ -2,29 +2,13 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Final Working LoRa Frequency Hopper
+# Title: Hopping (Corrected)
+# Generated: Thu Jul 24 17:26:10 2025
 ##################################################
 
-# --- Standard Python Imports ---
-import sys
-import traceback
-import ctypes
-
-# --- GNU Radio and WX Imports ---
-from gnuradio import gr
-from gnuradio import eng_notation
-from gnuradio import uhd
-from gnuradio import wxgui
-from gnuradio.fft import window
-from gnuradio.filter import firdes
-from gnuradio import filter # Import the full filter module
-from gnuradio.wxgui import fftsink2
-from grc_gnuradio import wxgui as grc_wxgui
-import wx
-import lora
-
-# --- X11 Threading Support (Standard GRC Boilerplate) ---
 if __name__ == '__main__':
+    import ctypes
+    import sys
     if sys.platform.startswith('linux'):
         try:
             x11 = ctypes.cdll.LoadLibrary('libX11.so')
@@ -32,103 +16,196 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio import uhd
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import lora
+import osmosdr
+import wx
 
-class HoppingReceiver(grc_wxgui.top_block_gui):
-    """
-    A GNU Radio flowgraph that receives LoRa signals, hopping between a
-    predefined list of frequencies at a regular interval.
-    """
+
+class single(grc_wxgui.top_block_gui):
+
     def __init__(self):
-        # STEP 1: Initialize the parent class FIRST. This is mandatory for WX.
-        # This call creates the GUI window and makes `self` a valid wx.EvtHandler.
-        grc_wxgui.top_block_gui.__init__(self, title="Final Working LoRa Hopper")
+        grc_wxgui.top_block_gui.__init__(self, title="Hopping")
 
-        #####################################################################
-        # STEP 2: Now that `self` is a valid WX object, define all variables
-        # and create all blocks, including the timer.
-        #####################################################################
-
-        # --- Hopping Configuration ---
-        self.target_freq_list = [
-            902.3e6, 902.5e6, 902.7e6, 902.9e6, 903.1e6, 903.3e6,
-            903.5e6, 903.7e6, 903.9e6, 904.1e6
-        ]
-        self.hop_interval_ms = 1000  # Time to wait on each frequency
-        self.current_freq_index = 0
-
-        # --- Basic LoRa and SDR Parameters ---
+        ##################################################
+        # Variables
+        ##################################################
         self.sf = sf = 7
-        self.bw = bw = 125000
         self.samp_rate = samp_rate = 1e6
+        self.bw = bw = 125000
+        
+        self.target_freq = target_freq = [902.3e6, 902.5e6, 902.7e6, 902.9e6, 903.1e6, 903.3e6, 903.5e6, 903.7e6, 903.9e6, 904.1e6]
+        
+        self.hop_interval = hop_interval = 1000 # 1 second
+        
+        self.freq_index = freq_index = 0
+
+        self.symbols_per_sec = symbols_per_sec = float(bw) / (2**sf)
+        self.firdes_tap = firdes_tap = firdes.low_pass(1, samp_rate, bw, 10000, firdes.WIN_HAMMING, 6.67)
+        self.downlink = downlink = False
+        self.decimation = decimation = 1
         self.capture_freq = capture_freq = 903e6
-        self.sdr_gain = sdr_gain = 30
-        
-        # --- Resampling Parameters ---
-        interp = 5
-        decim = 8
-        self.resampled_rate = samp_rate * interp / decim
+        self.bitrate = bitrate = sf * (1 / (2**sf / float(bw)))
 
-        # --- Block Creation ---
-        self.uhd_source = uhd.usrp_source("serial=3134B8C", uhd.stream_args(cpu_format="fc32", channels=range(1)))
-        self.resampler = filter.rational_resampler_ccc(interp, decim, None, None)
-        self.lora_receiver = lora.lora_receiver(self.resampled_rate, capture_freq, [self.target_freq_list[0]], bw, sf, crc=True, implicit=False)
-        self.fft_sink = fftsink2.fft_sink_c(self.GetWin(), baseband_freq=capture_freq, sample_rate=samp_rate, fft_size=1024, title='FFT Plot')
-        self.Add(self.fft_sink.win)
-        self.socket_sink = lora.message_socket_sink('127.0.0.1', 40868, 0)
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_fftsink2_1 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=capture_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_1.win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("serial=3134B8C", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(capture_freq, 0)
+        self.uhd_usrp_source_0.set_gain(20, 0)
         
-        # --- Timer Creation and Binding ---
+        # REMOVED: This block was the source of the crash. It was defined
+        # but never connected, and it used a module name 'filter' that was
+        # not imported, causing the __init__ method to fail.
+        # self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(...)
+
+        self.lora_message_socket_sink_0 = lora.message_socket_sink('127.0.0.1', 40868, 0)
+
+        self.lora_lora_receiver_0 = lora.lora_receiver(samp_rate, capture_freq, ([self.target_freq[self.freq_index]]), bw, sf, False, 4, True, False, downlink, decimation, False, False)
+        
+        ##################################################
+        # Timer for frequency hopping
+        ##################################################
         self.hop_timer = wx.Timer(self, wx.ID_ANY)
-        self.Bind(wx.EVT_TIMER, self._handle_hop_timer, self.hop_timer)
-
-        ##################################################
-        # Set Block Parameters
-        ##################################################
-        self.uhd_source.set_samp_rate(samp_rate)
-        self.uhd_source.set_center_freq(capture_freq, 0)
-        self.uhd_source.set_gain(sdr_gain, 0)
+        self.Bind(wx.EVT_TIMER, self._on_hop_timer, self.hop_timer)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.uhd_source, 0), (self.resampler, 0))
-        self.connect((self.resampler, 0), (self.lora_receiver, 0))
-        self.connect((self.uhd_source, 0), (self.fft_sink, 0))
-        self.msg_connect((self.lora_receiver, 'frames'), (self.socket_sink, 'in'))
+        self.msg_connect((self.lora_lora_receiver_0, 'frames'), (self.lora_message_socket_sink_0, 'in'))
+        self.connect((self.uhd_usrp_source_0, 0), (self.lora_lora_receiver_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_fftsink2_1, 0))
 
-    def Start(self, top_level_start=True):
-        """
-        Overrides the default Start method. This method is called twice:
-        1. With top_level_start=False by the parent's __init__.
-        2. With top_level_start=True by our main() function.
-        """
-        # Always call the parent's Start method.
-        super(HoppingReceiver, self).Start(top_level_start)
-        
-        # We ONLY start our custom timer if this is the "real" start from main().
-        # This avoids the race condition that caused all the previous errors.
-        if top_level_start and self.hop_interval_ms > 0:
-            print("INFO: Starting frequency hopping timer with interval: %dms" % self.hop_interval_ms)
-            self.hop_timer.Start(self.hop_interval_ms)
+    def Start(self, *args, **kwargs):
+        super(single, self).Start(*args, **kwargs)
+        if self.hop_interval > 0:
+            self.hop_timer.Start(self.hop_interval)
 
-    def _handle_hop_timer(self, event):
-        """This function is called every time the hop_timer fires."""
-        self.current_freq_index = (self.current_freq_index + 1) % len(self.target_freq_list)
-        new_freq = self.target_freq_list[self.current_freq_index]
-        self.lora_receiver.set_frequencies([new_freq])
-        print("Hopping to: %.3f MHz" % (new_freq / 1e6))
+    def _on_hop_timer(self, event):
+        self.freq_index = (self.freq_index + 1) % len(self.target_freq)
+        new_freq = self.target_freq[self.freq_index]
+        self.lora_lora_receiver_0.set_frequencies([new_freq])
+        print("Hopping to frequency: %.2f MHz" % (new_freq / 1e6))
+
+    # ... (rest of the getter/setter methods are unchanged) ...
+    def get_sf(self):
+        return self.sf
+
+    def set_sf(self, sf):
+        self.sf = sf
+        self.set_symbols_per_sec(float(self.bw) / (2**self.sf))
+        self.lora_lora_receiver_0.set_sf(self.sf)
+        self.set_bitrate(self.sf * (1 / (2**self.sf / float(self.bw))))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_1.set_sample_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.set_firdes_tap(firdes.low_pass(1, self.samp_rate, self.bw, 10000, firdes.WIN_HAMMING, 6.67))
+
+    def get_bw(self):
+        return self.bw
+
+    def set_bw(self, bw):
+        self.bw = bw
+        self.set_symbols_per_sec(float(self.bw) / (2**self.sf))
+        self.set_firdes_tap(firdes.low_pass(1, self.samp_rate, self.bw, 10000, firdes.WIN_HAMMING, 6.67))
+        self.set_bitrate(self.sf * (1 / (2**self.sf / float(self.bw))))
+
+    def get_target_freq(self):
+        return self.target_freq
+
+    def set_target_freq(self, target_freq):
+        self.target_freq = target_freq
+        self.freq_index = 0
+        self.lora_lora_receiver_0.set_frequencies([self.target_freq[self.freq_index]])
+
+    def get_symbols_per_sec(self):
+        return self.symbols_per_sec
+
+    def set_symbols_per_sec(self, symbols_per_sec):
+        self.symbols_per_sec = symbols_per_sec
+
+    def get_firdes_tap(self):
+        return self.firdes_tap
+
+    def set_firdes_tap(self, firdes_tap):
+        self.firdes_tap = firdes_tap
+
+    def get_downlink(self):
+        return self.downlink
+
+    def set_downlink(self, downlink):
+        self.downlink = downlink
+
+    def get_decimation(self):
+        return self.decimation
+
+    def set_decimation(self, decimation):
+        self.decimation = decimation
+
+    def get_capture_freq(self):
+        return self.capture_freq
+
+    def set_capture_freq(self, capture_freq):
+        # FIX: Corrected typo in argument name `capture__freq`
+        self.capture_freq = capture_freq
+        self.wxgui_fftsink2_1.set_baseband_freq(self.capture_freq)
+        self.uhd_usrp_source_0.set_center_freq(self.capture_freq, 0)
+
+    def get_bitrate(self):
+        return self.bitrate
+
+    def set_bitrate(self, bitrate):
+        self.bitrate = bitrate
 
 
-def main(top_block_cls=HoppingReceiver, options=None):
-    """Main function to create and run the flowgraph with robust error handling."""
+def main(top_block_cls=single, options=None):
+    # ADDED: A try/except block for better error reporting.
+    # This will catch the crash and print the exact error message.
     try:
         tb = top_block_cls()
         tb.Start(True)
         tb.Wait()
     except Exception as e:
-        print("\nFATAL: An error occurred while starting the flowgraph.")
-        print("------------------------------------------------------")
+        print "Error starting flowgraph: %s" % e
+        import traceback
         traceback.print_exc()
-        print("------------------------------------------------------")
 
 
 if __name__ == '__main__':
