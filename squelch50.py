a7a2b9da41f65042586d6b297095cda6d6e30bf5
tashaadler <60852757+tashaadler@users.noreply.github.com>
2025-10-21 12:11:47 -0400

    Change target and capture frequencies to 907.3 MHz

--- squelch50.py
+++ squelch50.py
@@ -48,14 +48,14 @@ class squelch50(grc_wxgui.top_block_gui):
         self.ch_bw = ch_bw = ch_rate/2
         self.bw = bw = 125000
         self.thresh = thresh = -90
-        self.target_freq = target_freq = 910.3e6
+        self.target_freq = target_freq = 907.3e6
         self.taps = taps = firdes.low_pass(1, samp_rate, ch_bw, ch_tb, firdes.WIN_HAMMING)
         self.symbols_per_sec = symbols_per_sec = float(bw) / (2**sf)
         self.firdes_tap = firdes_tap = firdes.low_pass(1, samp_rate, bw, 10000, firdes.WIN_HAMMING, 6.67)
         self.downlink = downlink = False
         self.delay = delay = int(150e3)
         self.decimation = decimation = 1
-        self.capture_freq = capture_freq = 910.3e6
+        self.capture_freq = capture_freq = 907.3e6
         self.bitrate = bitrate = sf * (1 / (2**sf / float(bw)))
         self.atten = atten = 30
 
